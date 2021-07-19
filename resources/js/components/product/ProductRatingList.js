ccf626f9f0f8c1621c6da830939ed7168f77fc1a
ngthhang <ngthhang9102000@gmail.com>
2021-07-19 12:36:28 +0700

    remove unused files

--- resources/js/components/product/ProductRatingList.js
+++ resources/js/components/product/ProductRatingList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { List, Typography, Space, Divider, Rate } from 'antd';
-import ProductRatingListPagination from './ProductRatingListPagination';
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 const data = [
   {
